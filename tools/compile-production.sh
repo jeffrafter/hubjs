cdd50e2170c027e6507bc15bffdb14ca1edb4e1e
Erich Ocean <erich.ocean@me.com>
2010-04-08 20:48:45 -0700

    increase sleep time to 3 seconds to try and prevent errors (sigh)

--- tools/compile-production.sh
+++ tools/compile-production.sh
@@ -7,6 +7,6 @@ cd src
 cat ../BUILD | grep "^[A-Za-z0-9_/]*.js$"  | grep -v "^debug[/]" | xargs -n 1 -P 1 node ../lib/strip_assertions.js > ../hub-production.js
 cd ..
 # for whatever reason, the pipe isn't fully flushed when the java command runs, resulting in an error
-sleep 1
+sleep 3
 java -jar tools/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar -o hub.min.js hub-production.js
 rm hub-production.js
